4e30233acc36865ccf54d0d84a2efb0f76899c16
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-28 01:09:27 -0400

    initial  commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ return str.split("").reverse("").join("");
 
 }
 
-console.log(reverseString("Hello"));
+console.log(reverseString("Anshul"));
 
 
 function isPalindrome(str){
@@ -12,35 +12,80 @@ function isPalindrome(str){
 }
 
 console.log(isPalindrome("oppo"));
+console.log(isPalindrome("level"));
+console.log(isPalindrome("racecar"));
 
 
 
-
-
-function convertDateToStr(date){
-    var dateStr = {day : "", month : "", year : ""};
+function getDateAsString(date){
+    var dateInStr = {day: '', month: '', year: ''};
+  
     if(date.day < 10){
-        dateStr.day  = '0'+ date.day 
-    }else{
-        dateStr.day = date.day.toString();
+      dateInStr.day = '0' + date.day;
+     }
+    else {
+      dateInStr.day = date.day.toString();
     }
+    
     if(date.month < 10){
-        dateStr.month = '0' + date.month
-    }else {
-        dateStr.month = date.month.toString();
+      dateInStr.month = '0' + date.month;
     }
-    dateStr.year = date.year.toString();
-    return dateStr ;
+    else {
+      dateInStr.month = date.month.toString();
+    }
+    
+    dateInStr.year = date.year.toString();
+    return dateInStr;
+  }
+  
+  var date = {day: 5, month: 2, year: 2020};
+  
+  console.log(getDateAsString(date));
 
-}
+
+  function getDateInAllFormats(date){
+    var ddmmyyyy = date.day + date.month + date.year;
+    var mmddyyyy = date.month + date.day + date.year;
+    var yyyymmdd = date.year + date.month + date.day;
+    var ddmmyy = date.day + date.month + date.year.slice(-2);
+    var mmddyy = date.month + date.day + date.year.slice(-2);
+    var yyddmm = date.year.slice(-2) + date.day + date.month;
+  
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
+  }
+  
+  var date = {
+    day: 20,
+    month: 6,
+    year: 2020
+  }
+  
+  console.log(getDateAsString(date));
+
+  
+  
+  function checkPalindromeForAllDateFormats(date) {
+    var dateFormatList = getDateInAllFormats(date);
+    var palindromeList = [];
+  
+    for (var i = 0; i < dateFormatList.length; i++) {
+      var result = isPalindrome(dateFormatList[i]);
+      palindromeList.push(result);
+    }
+    return palindromeList;
+  }
+  
+  var date = {
+    day: 11,
+    month: 2,
+    year: 2020
+  }
+  
+  var dateStr = getDateAsString(date);
+  
+  console.log(checkPalindromeForAllDateFormats(dateStr));
+
+  
 
 
 
-date ={
-    day : 5,
-    month : 9,
-    year : 2020
-};
-
-
-console.log(convertDateToStr(date));
